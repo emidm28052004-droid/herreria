102e926095b78d8c283767be7624e999188b2054
emidm28052004-droid <emidm28052004@gmail.com>
2026-06-25 21:11:00 -0300

    ahora si (creo)

--- main/JS/galeria.js
+++ main/JS/galeria.js
@@ -1,56 +1,57 @@
 const botones = document.querySelectorAll(".categoria");
 const imagenes = {
   todos: [
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/decoraciones/decoracion3.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/escaleras/escalera1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/mobiliarios/mobiliario2.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/puertasYportones/porton2.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/techos/techo1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/ventanas/ventana1.webp"
+    "../../imgFolder/Galeria/decoraciones/decoracion3.webp",
+    "../../imgFolder/Galeria/escaleras/escalera1.webp",
+    "../../imgFolder/Galeria/mobiliarios/mobiliario2.webp",
+    "../../imgFolder/Galeria/puertasYportones/porton2.webp",
+    "../../imgFolder/Galeria/techos/techo1.webp",
+    "../../imgFolder/Galeria/ventanas/ventana1.webp"
   ],
   decoraciones: [
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/decoraciones/decoracion1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/decoraciones/decoracion2.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/decoraciones/decoracion3.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/decoraciones/decoracion4.webp",
+    "../../imgFolder/Galeria/decoraciones/decoracion1.webp",
+    "../../imgFolder/Galeria/decoraciones/decoracion2.webp",
+    "../../imgFolder/Galeria/decoraciones/decoracion3.webp",
+    "../../imgFolder/Galeria/decoraciones/decoracion4.webp",
     "#",
     "#"
   ],
   escaleras: [
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/escaleras/escalera1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/escaleras/escalera2.webp",
+    "../../imgFolder/Galeria/escaleras/escalera1.webp",
+    "../../imgFolder/Galeria/escaleras/escalera2.webp",
     "#",
     "#",
     "#",
     "#"
   ],
   mobiliarios: [
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/mobiliarios/mobiliario1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/mobiliarios/mobiliario2.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/mobiliarios/mobiliario3.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/mobiliarios/mobiliario4.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/mobiliarios/mobiliario5.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/mobiliarios/mobiliario6.webp"
-  ],rtasYportones: [
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/puertasYportones/porton1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/puertasYportones/porton2.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/puertasYportones/porton3.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/puertasYportones/puerta1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/puertasYportones/puerta2.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/puertasYportones/puerta3.webp",
+    "../../imgFolder/Galeria/mobiliarios/mobiliario1.webp",
+    "../../imgFolder/Galeria/mobiliarios/mobiliario2.webp",
+    "../../imgFolder/Galeria/mobiliarios/mobiliario3.webp",
+    "../../imgFolder/Galeria/mobiliarios/mobiliario4.webp",
+    "../../imgFolder/Galeria/mobiliarios/mobiliario5.webp",
+    "../../imgFolder/Galeria/mobiliarios/mobiliario6.webp"
+  ],
+  puertasYportones: [
+    "../../imgFolder/Galeria/puertasYportones/porton1.webp",
+    "../../imgFolder/Galeria/puertasYportones/porton2.webp",
+    "../../imgFolder/Galeria/puertasYportones/porton3.webp",
+    "../../imgFolder/Galeria/puertasYportones/puerta1.webp",
+    "../../imgFolder/Galeria/puertasYportones/puerta2.webp",
+    "../../imgFolder/Galeria/puertasYportones/puerta3.webp",
   ],
   techos: [
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/techos/techo1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/techos/techo2.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/techos/techo3.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/techos/techo4.webp",
+    "../../imgFolder/Galeria/techos/techo1.webp",
+    "../../imgFolder/Galeria/techos/techo2.webp",
+    "../../imgFolder/Galeria/techos/techo3.webp",
+    "../../imgFolder/Galeria/techos/techo4.webp",
     "#",
     "#"
   ],
   ventanas: [
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/ventanas/ventana1.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/ventanas/ventana2.webp",
-    "https://emidm28052004-droid.github.io/herreria/imgFolder/Galeria/ventanas/ventana3.webp",
+    "../../imgFolder/Galeria/ventanas/ventana1.webp",
+    "../../imgFolder/Galeria/ventanas/ventana2.webp",
+    "../../imgFolder/Galeria/ventanas/ventana3.webp",
     "#",
     "#",
     "#"
